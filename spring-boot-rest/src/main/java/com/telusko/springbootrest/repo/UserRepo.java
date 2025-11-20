d1352c9376d15e52773e24cf1ac090af57b3b06c
chaitanya275 <syamalachaitanya2003@gmail.com>
2025-11-20 15:24:58 +0530

    Add JobApp Project

---
+++ spring-boot-rest/src/main/java/com/telusko/springbootrest/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package com.telusko.springbootrest.repo;
+
+import com.telusko.springbootrest.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo  extends JpaRepository<User,Integer> {
+    User findByUsername(String username);
+}
